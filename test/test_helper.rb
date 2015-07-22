ae6c3baad23595582e88a014b16d88968095c81a
Dave Morse <dcmorse@gmail.com>
2015-07-21 23:49:45 -0400

    get rake passing the default tests.Install rspec

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,12 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+
+# Included per demand of https://github.com/plataformatec/devise#test-helpers
+# via http://stackoverflow.com/questions/4308094/all-ruby-tests-raising-undefined-method-authenticate-for-nilnilclass
+# via http://benincosa.com/?p=2965
+# -dm 07-21-2015
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
