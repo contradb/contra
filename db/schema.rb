0b50779c6f73c4ab4998c586e49f49aa80e00381
Dave Morse <dcmorse@gmail.com>
2015-07-25 18:22:39 -0400

    create choreographers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720223311) do
+ActiveRecord::Schema.define(version: 20150725221456) do
+
+  create_table "choreographers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "choreographers", ["name"], name: "index_choreographers_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
