367e0c8375d1b6df44afe7e92e6c3041f74c9051
Dave Morse <dcmorse@gmail.com>
2015-12-16 09:11:52 -0500

    submit test

--- spec/features/users/create_spec.rb
+++ spec/features/users/create_spec.rb
@@ -2,13 +2,27 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe 'Creating user from welcome page' do
-  it "goes to the Sign Up page on success" do
+  it "Splash page links to Sign Up page" do
     visit '/'
     click_link "Sign Up"
-    # assumes logged in, but I don't know how to do that yet -dm 12-16-2015
     expect(page).to have_content("Sign Up")
     expect(page).to have_content("Email:")
     expect(page).to have_content("Name:")
     expect(page).to have_content("Password:")
   end
+
+  it "Sign Up page works" do
+    visit '/users/sign_up'
+    expect(page).to have_content("Sign Up")
+    expect(page).to have_content("Email:")
+    expect(page).to have_content("Name:")
+    expect(page).to have_content("Password:")
+    fill_in "user_email",                 with: "forgedmail@bog.us"
+    fill_in "user_name",                  with: "Forgedmail at Bog.us"
+    fill_in "user_password",              with: "testing password 1234"
+    fill_in "user_password_confirmation", with: "testing password 1234"
+    click_button "Sign Up"
+  end
+
+
 end
