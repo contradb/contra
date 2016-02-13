9b748286c29523ac948afb043cb0fc431a86cc7a
Dave Morse <dcmorse@gmail.com>
2016-02-13 00:04:30 -0500

    fix outdated test, thus fixing https://github.com/dcmorse/contra/issues/87

--- spec/features/programs/create_spec.rb
+++ spec/features/programs/create_spec.rb
@@ -15,7 +15,7 @@ describe 'Creating program from index page' do
     visit '/programs'
     click_link "New Program"
 
-    expect(page).to have_selector("input[value='New Program']")
+    expect(page).to have_selector("input[placeholder='Program Title']")
     scrutinize_layout page
   end
 
