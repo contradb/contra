d8d15ac4229f0c71e2605b015459d12ba0e8183b
David Morse <dcmorse@gmail.com>
2016-02-18 01:48:53 -0500

    figure OO brainstorm

--- mockup/figure.rb
+++ mockup/figure.rb
@@ -1,6 +1,7 @@
 class Figure
   def pvalues () [] end
-  def Figure.ptypes () [] end
+  def self.ptypes () [] end
+  def self.pdefaults () [] end
 end
 
 class SerialFigure < Figure
@@ -12,84 +13,129 @@ end
 # ________________________________________________________________
 
 class Parameter
-  # subclasses may override self.Default to give a default:
-  def self.Default () nil end
 end
 
-class PTurn < Parameter; end
-  class PTurnPlus270  < PTurn; def self.Default () +270 end end
-  class PTurnPlus360  < PTurn; def self.Default () +360 end end
-  class PTurnPlus540  < PTurn; def self.Default () +540 end end
-  class PTurnMinus270 < PTurn; def self.Default () -270 end end
-  class PTurnMinus360 < PTurn; def self.Default () -360 end end
-  class PTurnMinus540 < PTurn; def self.Default () -540 end end
-
-
-module ConcreteWho
-  def self.Default () self end
+class PTurn < Parameter;
 end
-
-class PWho                 < Parameter  ; end
-  class P1Dancer           < PWho       ; end # {1},{3}
-    class PGentlespoon1    < P1Dancer ; def self.Default () self end end
-    class PGentlespoon2    < P1Dancer ; def self.Default () self end end
-    class PLadle1          < P1Dancer ; def self.Default () self end end
-    class PLadle2          < P1Dancer ; def self.Default () self end end
-  class P2Dancers          < PWho       ; end # {2},{1},{1}
-    class POnes            < P2Dancers; def self.Default () self end end
-    class PTwos            < P2Dancers; def self.Default () self end end
-    class PGentlespoons    < P2Dancers; def self.Default () self end end
-    class PLadles          < P2Dancers; def self.Default () self end end
-    class PFirstCorners    < P2Dancers; def self.Default () self end end # G1L2
-    class PSecondCorners   < P2Dancers; def self.Default () self end end # G2L1
-  class P4Dancers          < PWho       ; end # {4}
-  class P2Pairs            < PWho       ; end # {2},{2}
-    class PPartners        < P2Pairs  ; def self.Default () self end end
-    class PNeighbors       < P2Pairs  ; def self.Default () self end end
-    class PSameRoles       < P2Pairs  ; def self.Default () self end end # G1G2,L1L2
+# clockwise is positive so that circle-right is dealing with positive
+# quantities and swings go in the positive direction.
 
 
+
+
+# PWho says which dancers are acted on by the other parameters.
+# Semantics are set by Figures, but these establish grouping and for
+# those parameters. Consider the allemande orbit figure - in some ways
+# the most complicated figure. It takes three parameters:
+# some subclass of P22Dancers, PTurnMinus540 and PTurnPlus180.
+# The first PTurn is the amount of turning for the inner couple,
+# and the second PTurn is the turning for the
+# outter couple. 
+# P1Dancer subclasses could likewise be used to establish 3&1
+# commands, like Lady1 into the center and be a bird, everybody else
+# circle round. 
+
+class PWho < Parameter; end
+class FROGS < PWho; end
+class PPairOrPairs < PWho; end
+class P1Dancer       < PWho; end # {1},{3} (1-2 total commands)
+class P2Dancers      < PPairOrPairs; end # {1},{3} (1-2 total commands)
+class P2x2Dancers    < PPairOrPairs; end # {2&2}   (1 total command, sent to both, e.g. partner swing)
+class P4Dancers      < PWho; end # {4}     (1 total command)
+# these classes aren't exhaustive, consider the pathological figure
+# Ones: swing, L2: spin around clockwise in place, G2 slide left.
+# So if there is a user-request for that busy a figure: extend as needed. 
+
+class PGentlespoon1  < P1Dancer    ; end
+class PGentlespoon2  < P1Dancer    ; end
+class PLadle1        < P1Dancer    ; end
+class PLadle2        < P1Dancer    ; end
+class POnes          < P2Dancers   ; end
+class PTwos          < P2Dancers   ; end
+class PGentlespoons  < P2Dancers   ; end
+class PLadles        < P2Dancers   ; end
+class PFirstCorners  < P2Dancers   ; end # G1L2
+class PSecondCorners < P2Dancers   ; end # G2L1
+class PPartners      < P2x2Dancers ; end
+class PNeighbors     < P2x2Dancers ; end
+class PSameRoles     < P2x2Dancers ; end # G1G2,L1L2
 
 # ________________________________________________________________
 
-class Turny < Figure
-  def initialize(degrees=self.class.GetPTurn.Default)
+## outdated section
+
+# Interface TurningFigure < Figure
+# * initialize takes PTurn
+# * implement pvalues method: appends PTurn onto super
+# * implement self.ptypes class method: appends self.pturn onto super
+# * implement self.pdefaults class method: appends defaults/nil onto super
+
+# Interface SubjectsFigure < Figure
+# * initialize takes pwho, default value self.class.GetPSubjects.default???
+# * implement pvalues method: appends PWho onto super
+# * implement self.ptypes class method: appends self.GetPSubject onto super???
+# * implement self.pdefaults class method: appends defaults/nil onto super
+
+# ________________________________________________________________
+
+
+
+
+class VariableTurnHelperFigure < Figure
+  def self.ptypes() super << PTurn end
+  def self.pdefaults() super << pturn_default end
+  def pvalues() super << @degrees end
+  def self.pturn_default() 360 end # may be overridden
+  def initialize(degrees=self.class.pturn_default)
+    raise "type error" unless degrees.is_a? Integer
     @degrees=degrees
   end
-  # subclasses implement self.GetPTurn for defaulting
-  def pvalues ()
-    super << @degrees
-  end
-  def self.ptypes ()
-    super << self.GetPTurn
+end
+
+class Circle < VariableTurnHelperFigure; end
+class Circle3Places < Circle; def self.pturn_default() 270 end end
+class CircleRight < Circle; def self.pturn_default() -360 end end
+class CircleRight3Places < CircleRight; def self.pturn_default() -270 end end
+
+class Star < VariableTurnHelperFigure; end
+class StarRight < Star; end
+class StarLeft < Star; def self.pturn_default() -360 end end
+
+class Gyre < VariableTurnHelperFigure
+  def self.ptypes() super << pwho end
+  def self.pdefaults() super << pwho_default end
+  def pvalues() super << @subject end
+  def self.pwho() PPairOrPairs end
+  def self.pwho_default() PPartners end
+  def initialize(subject=self.class.pwho_default, degrees=self.class.pturn_default)
+    raise "type error" unless degrees.is_a? Integer
+    raise "type error" unless subject.is_a? Class and self.class.pwho > subject
+    @subject = subject
+    @degrees = degrees
   end
 end
 
-# ________________________________________________________________
+class GyreLeftShoulder < Gyre; def self.pturn_default() super * -1 end end
 
-
-
-
-class Circle < Turny
-  def self.GetPTurn ()
-    PTurnPlus360
+class AllemandeAndOrbit < Figure
+  def self.pwho_inner() P2Dancers end
+  def self.ptypes()   super << P2Dancers  << PTurn << PTurn end
+  def self.pdefault() super << pwho_inner << -540  << 180 end
+  def pvalues() super << @who_inner << @deg_inner << @deg_outer end
+  def initialize(who_inner, deg_inner=self.class.pturn_inner, deg_outer=self.class.pturn_outer)
+    raise "type error" unless who_inner < pwho_inner
+    raise "type error" unless deg_inner.is_a? Integer
+    raise "type error" unless deg_outer.is_a? Integer
+    raise "inners and outers going same direction" unless deg_inner * deg_outer < 0
+    @who_inner = who_inner
+    @deg_inner = deg_inner
+    @deg_outer = deg_outer
   end
 end
 
-class StarRight < Turny
-  def self.GetPTurn ()
-    PTurnPlus360
-  end
-end
 
-class StarLeft < Turny
-  def self.GetPTurn ()
-    PTurnMinus360
-  end
-end
+# Here's the thing: I think this initialize business is waste of time.
+# We need pvalues=(class &rest rest) that takes rest, subs in defaults for nil, and stores it.
+# initialize is set once at Figure and just calls pvalue=. Otherwise it's simple.
+# Finally we need a pchoices class method that describes valid UI choices for parameters.
 
-class Gyre < Turny
-  def self.GetPTurn ()
-    PTurnPlus360
-  end
-end
