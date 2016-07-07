4e7d454735134198ba4ddec8fb2ddf2c6592d90e
David Morse <dcmorse@gmail.com>
2016-07-06 21:11:55 -0500

    javascript testing works

--- spec/features/dances/create_spec.rb
+++ spec/features/dances/create_spec.rb
@@ -3,24 +3,53 @@ require 'login_helper'
 require 'support/scrutinize_layout'
 
 
-describe 'Creating dances' do
-  it "creates a new dance with non-javascript data", js: true do
+describe 'Creating dances', js: true  do
+  it 'creates a new dance with non-javascript data' do
     with_login do
       visit '/dances/new'
       expect(page.text).to include 'There\'s a lot of ink spilled over "gentlemen" versus "men" versus "leads".'
-      fill_in "dance_title", with: "Call Me"
-      fill_in "dance[choreographer_name]", with: "Cary Ravitz"
-      fill_in "dance[start_type]", with: "improper"
-      click_button "Save Dance"
+      fill_in 'dance_title', with: 'Call Me'
+      fill_in 'dance[choreographer_name]', with: 'Cary Ravitz'
+      fill_in 'dance[start_type]', with: 'improper'
+      click_button 'Save Dance'
       
-      expect(page).to have_css("h1", text: "Call Me")
-      expect(page).to have_content("Cary Ravitz")
-      expect(page).to have_content("improper")
+      expect(page).to have_css('h1', text: 'Call Me')
+      expect(page).to have_content('Cary Ravitz')
+      expect(page).to have_content('improper')
     end
   end
 
-  pending "creates a new dance with some interesting figures"
+  it 'has some working javascript' do
+    with_login do
+      visit '/dances/new'
+      expect(page.text).to include 'There\'s a lot of ink spilled over "gentlemen" versus "men" versus "leads".'
+      fill_in 'dance_title', with: 'Call Me'
+      fill_in 'dance[choreographer_name]', with: 'Cary Ravitz'
+      fill_in 'dance[start_type]', with: 'improper'
+      
+      7.times { click_button 'Remove Figure' }
 
-  pending "tests choreographer auto-complete"
+      click_button 'Save Dance'
+
+      expect(page).to have_css('h1', text: 'Call Me')
+      expect(page).to have_content('A1')
+      expect(page).to_not have_content('A2')
+      expect(page).to_not have_content('B1')
+      expect(page).to_not have_content('B2')
+
+      click_on 'Edit'
+
+      2.times { click_button 'Add Figure' }
+
+      click_button 'Save Dance'
+
+      expect(page).to have_content('A1')
+      expect(page).to have_content('A2')
+      expect(page).to_not have_content('B1')
+      expect(page).to_not have_content('B2')
+    end
+  end
+
+  pending 'tests choreographer auto-complete'
 
 end
