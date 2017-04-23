8d08c8a81f05cdaa3fdd587edefce8a26189f862
Dave Morse <dcmorse@gmail.com>
2017-04-23 01:14:37 -0400

    update production secrets for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,19 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = {host: 'contradb.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.secrets.admin_gmail_username,
+    password:             Rails.application.secrets.admin_gmail_password,
+    authentication:       :login,
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -76,6 +89,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  
 end
+
