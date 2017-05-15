ceb5bcbe8e4be34612958b3957641e9a280b4d58
Dave Morse <dcmorse@gmail.com>
2017-05-14 19:33:03 -0500

    choreographer extra info #162

---
+++ spec/features/choreographers/index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Choreographer index' do
+  it "Displays a choreographer's information" do
+    choreographer = FactoryGirl.create(:friendly_choreographer)
+
+    visit choreographers_path
+
+    expect(page).to have_link(choreographer.name, href: choreographer_path(choreographer))
+    expect(page).to have_link(choreographer.website)
+    expect(page).to have_text(choreographer.publish.titleize)
+    expect(page).to have_link(choreographer.website_text, href: choreographer.website_url)
+  end
+end
