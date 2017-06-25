0639f874fb4c225659cc8828beb989e0e57e206f
Dave Morse <dcmorse@gmail.com>
2017-06-24 23:25:41 -0500

    use rails 5 'or' scope on dance readability

--- app/models/dance.rb
+++ app/models/dance.rb
@@ -14,7 +14,7 @@ class Dance < ApplicationRecord
     elsif user.admin?
       all
     else
-      where('publish= true OR user_id= ?', user.id)
+      where(publish: true).or(where(user_id: user.id))
     end
   }
 
