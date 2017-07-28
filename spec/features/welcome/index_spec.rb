bf71936b02e11ad636380003a74380a50eaf990a
Dave Morse <dcmorse@gmail.com>
2017-07-28 05:13:32 -0500

    write first spec

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,15 @@
+# coding: utf-8
+require 'rails_helper'
+require 'login_helper'
+
+describe 'Welcome page', js: true do
+  let (:dance) {FactoryGirl.create(:dance)}
+  it 'displays dance columns' do
+    dance
+    visit '/'
+    expect(page).to have_text(dance.title)
+    expect(page).to have_text(dance.choreographer.name)
+    # TODO
+    # expect(page).to have_text(dance.user.name) 
+  end
+end
