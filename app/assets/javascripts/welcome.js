b1b545648fdc15231d1b08d54009c799ea1ac0b4
Dave Morse <dcmorse@gmail.com>
2017-09-16 16:44:27 -0500

    WIP on dynamic html for figure filter

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,57 @@
+  function installEventHandlers(selector) {
+    selector.find('.figure-filter-op').onChange(filterOpChanged);
+    selector.find('.figure-filter-add').onClick(filterAddSubfilter);
+  }
+  
+  function filterOpChanged(e) {
+    // xNonsenseSubfilters();
+    updateQuery();
+  }
+
+  function filterAddSubfilter(e) {
+    // add the subfilter
+    updateQuery();
+  }
+
+  function filterX(e) {         // delete filter
+    // if (it makes sense)
+    // remove the filter
+    updateQuery();
+  }
+
+  var updateQuery;              // defined below...
+
+  function buildFilter(figure_filter) {
+    figure_filter = $(figure_filter);
+    var op = figure_filter.children('.figure-filter-op').val();
+    console.log('op = '+op);
+    if (op === 'figure') {
+       return [op, figure_filter.children('.figure-move').val()];
+    } else {
+      var kids = figure_filter.children('.figure-filter').get();
+      if (!Array.prototype.map) { throw "I was expecting Array.map to be defined"; }
+      var filter = kids.map(buildFilter);
+      filter.unshift(op);
+      console.log(filter);
+      return filter;
+    }
+  }
+
+
+
 jQuery(document).ready(function() {
+
+
+  // ================================================================
+
+  updateQuery = function() {
+    $('#query-buffer').val(JSON.stringify(buildFilter($('#figure-filter-root'))));
+    if (dataTable) {
+      dataTable.draw(); 
+    }
+  }
+  updateQuery();
+
   if (!Array.isArray) {
     Array.isArray = function(arg) {
       return Object.prototype.toString.call(arg) === '[object Array]';
@@ -25,9 +78,9 @@ jQuery(document).ready(function() {
           "ajax": {
             url: $('#dances-table').data('source'),
             data: function(d) {
-              // d.excludeMoves = $('#exclude-moves').val();
               // d.figureQuery = arrayToObject(['and', ['none', ['figure', 'gyre']], ['follows', ['figure', 'roll away'], ['figure', 'swing']]]);
-              d.figureQuery = arrayToObject(['and']);
+              d.figureQuery = arrayToObject(JSON.parse($('#query-buffer').val()));
+              // d.figureQuery = arrayToObject(['and']);
             }
           },
           "pagingType": "full_numbers",
