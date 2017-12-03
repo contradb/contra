7eed491c3d8adf2efba6832dc2cb6e1975542bb7
Dave Morse <dcmorse@gmail.com>
2017-12-03 08:06:34 -0600

    fix bug in user settings edit spec by filling out password

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -3,19 +3,19 @@ require 'login_helper'
 require 'support/scrutinize_layout'
 
 describe 'Editing user' do
-  xit 'editing email works' do  # but it doesn't! But in real life it does now. Hm...-dm 12-02-2017
+  it 'editing email works' do  # but it doesn't! But in real life it does now. Hm...-dm 12-02-2017
     password = 'smurfs4eva'
     with_login(password: password) do |user|
       visit edit_user_registration_path
       scrutinize_layout page
+      fill_in 'user_name', with: 'Yahoo Serious'
       fill_in 'user_email', with: 'serious@yahoo.com'
-      # fill_in 'user_current_password', with: password
-      # click_on('update-user')
-      # binding.pry
-      find('button.btn-success').click
-      expect(page).to have_text('Your account has been updated successfully.')
+      fill_in 'user_current_password', with: password
+      find('button.btn-success').click # click_on('update-user')
       user.reload
+      expect(user.name).to eq('Yahoo Serious')
       expect(user.email).to eq('serious@yahoo.com')
+      expect(page).to have_text('Your account has been updated successfully.')
     end
   end
 
