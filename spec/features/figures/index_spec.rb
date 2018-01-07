74f479f86afcb3095f49b959aa7a99e6400feb69
Dave Morse <dcmorse@gmail.com>
2018-01-06 23:48:40 -0600

    broken spec for preference integration with figures index

--- spec/features/figures/index_spec.rb
+++ spec/features/figures/index_spec.rb
@@ -9,4 +9,13 @@ describe 'figures index' do
     expect(page).to have_link('do si do', href: figure_path('do-si-do'))
     expect(page).to have_link("Rory O'Moore", href: figure_path('rory-omoore'))
   end
+
+  it 'is preference aware' do
+    JSLibFigure.with_prefs(JSLibFigure.test_prefs) do
+      visit figures_path
+      expect(page).to have_link('swing', href: figure_path('swing'))
+      expect(page).to have_link('almond', href: figure_path('allemande'))
+      expect(page).to have_link('do si do left shoulder', href: figure_path('see saw'))
+    end
+  end
 end
