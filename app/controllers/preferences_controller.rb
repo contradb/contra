8672ba580470e493557700a59085edaff0477da8
Dave Morse <dcmorse@gmail.com>
2018-02-04 11:49:42 -0600

    WIP spike on form naming - code will need to be unwound

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -8,7 +8,7 @@ class PreferencesController < ApplicationController
   def update
     @preferences_form = PreferencesForm.new(preferences_form_params)
     if @preferences_form.save
-      redirect_to(root_url, notice: 'preferences updated')
+      redirect_to(root_url, notice: "preferences updated #{preferences_form_params.dig(:grabonzo, :beans)}")
     else
       render :edit
     end
@@ -17,6 +17,7 @@ class PreferencesController < ApplicationController
   private
   # Never trust parameters from the scary internet, only allow the white list through.
   def preferences_form_params
-    params.require(:preferences_form).permit(:name)
+    binding.pry
+    params.require(:preferences_form).permit(:name, {grabonzo: :beans})
   end
 end
--- app/forms/preferences_form.rb
+++ app/forms/preferences_form.rb
@@ -2,6 +2,25 @@ class PreferencesForm
   include ActiveModel::Model
   attr_accessor :name
 
+  # has_many 'Preference::Preferences'
+  # accepts_nested_attributes_for 'Preference::Preferences'
+
+  def x_attributes=(attributes)
+    binding.pry
+  end
+
+  def xs
+    @xs ||= [Preference::Move.new(term: 'gyre', substitution: 'darcy'), Preference::Dancer.new(term: 'ladles', substitution: 'ravens')]
+  end
+
+
+  def p0
+    @preference0 || Preference::Move.new
+  end
+  def p0=(value)
+    @preference0 = value
+  end
+
   def save
     valid? or return false
     # ActiveRecord::Base.transaction do
