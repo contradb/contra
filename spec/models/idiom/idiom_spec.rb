eedf33d12d775efd58b7de2efa3c22cec075d0da
Dave Morse <dcmorse@gmail.com>
2018-03-23 19:41:36 -0500

    User#idioms dependent destroy spec

--- spec/models/idiom/idiom_spec.rb
+++ spec/models/idiom/idiom_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Idiom, type: :model do
+
   it '#user' do
     user = FactoryGirl.build(:user)
     idiom = FactoryGirl.build(:move_idiom, user: user) # use move_idiom because idiom is abstract
     expect(idiom.user).to be(user)
+  end
+
+  it 'User#destroy also destroys idioms' do
+    user = FactoryGirl.create(:user)
+    idiom = FactoryGirl.create(:move_idiom, user: user) # use move_idiom because idiom is abstract
+    user.destroy!
+    expect(Idiom::Idiom.find_by(id: idiom.id)).to eq(nil)
   end
 end
