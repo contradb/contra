9a01af4d81bc73d4323a1e9d99d6cbfd65e63401
Dave Morse <dcmorse@gmail.com>
2018-03-29 20:10:29 -0500

    fix spec

--- spec/views/figures/index.html.erb_spec.rb
+++ spec/views/figures/index.html.erb_spec.rb
@@ -10,11 +10,12 @@ RSpec.describe "figures/index", type: :view do
     assign(:move_terms_and_substitutions, JSLibFigure.move_terms_and_substitutions(dialect))
     assign(:mdtab, Move.mdtab([], dialect))
     render
-    expect(rendered).to have_content('allemande orbit almond balance')
+    expect(rendered).to have_content('allemande orbit almond arch & dive balance')
     expect(rendered).to have_content('custom darcy do si do')
     expect(rendered).to have_link('allemande orbit', href: '/figures/allemande-orbit')
     expect(rendered).to have_link('almond', href: '/figures/allemande')
     expect(rendered).to have_link('balance', href: '/figures/balance')
+    expect(rendered).to have_link('arch & dive', href: '/figures/arch-and-dive')
     expect(rendered).to have_link('custom', href: '/figures/custom')
     expect(rendered).to have_link('darcy', href: '/figures/gyre')
     expect(rendered).to have_link('do si do', href: '/figures/do-si-do')
