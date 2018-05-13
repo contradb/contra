f67239045a8e387233c9b7778412a4eee9c168a4
Dave Morse <dcmorse@gmail.com>
2018-05-13 18:38:06 -0500

    edit contact info is polished

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -1,21 +1,31 @@
 require 'rails_helper'
 require 'login_helper'
 require 'support/scrutinize_layout'
+include Warden::Test::Helpers
 
 describe 'Editing user' do
-  it 'editing email works' do
-    password = 'smurfs4eva'
-    with_login(password: password) do |user|
+  it 'editing Identity works' do
+    current_password = 'smurfs4eva'
+    new_password = 'smurfs are so last year'
+    with_login(password: current_password) do |user|
       visit edit_user_registration_path
       scrutinize_layout page
       fill_in 'user_name', with: 'Yahoo Serious'
       fill_in 'user_email', with: 'serious@yahoo.com'
-      fill_in 'user_current_password', with: password
+      fill_in 'user_current_password', with: current_password
+      fill_in 'user_password', with: new_password
+      fill_in 'user_password_confirmation', with: new_password
       click_button 'Update Identity'
       expect(page).to have_text('Your account has been updated successfully.')
       user.reload
       expect(user.name).to eq('Yahoo Serious')
       expect(user.email).to eq('serious@yahoo.com')
+      logout(:user)
+      visit new_user_session_path
+      fill_in 'user_email', with: 'serious@yahoo.com'
+      fill_in 'user_password', with: new_password
+      click_button 'Login'
+      expect(page).to have_content('Signed in successfully')
     end
   end
 
