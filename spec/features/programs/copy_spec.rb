8c46f5766519ab3207eb4be55ac77d81ce2b3a81
Dave Morse <dcmorse@gmail.com>
2018-06-30 23:57:56 -0500

    improve program spec

--- spec/features/programs/copy_spec.rb
+++ spec/features/programs/copy_spec.rb
@@ -5,7 +5,7 @@ require 'support/scrutinize_layout'
 include Warden::Test::Helpers
 Warden.test_mode!
 
-describe 'Copying programs' do
+describe 'Copying programs', js: true do
   let :program {FactoryGirl.create(:program,
                                    title: "Fish Dance",
                                    dances: [:box_the_gnat_contra, :call_me].map {|sym| FactoryGirl.create(sym)},
@@ -16,13 +16,21 @@ describe 'Copying programs' do
     login_as(FactoryGirl.create(:user))
   end
 
-  it "copies the title" do
-    visit program_path(program)
-    click_link 'Copy'
-
-    scrutinize_layout page
+  it "copies the program" do
+    visit new_program_path(copy_program_id: program.id)
     expect(page).to have_selector("input[value='Fish Dance copy']")
+    click_button 'Save Program'
+    expect(page).to have_content('Program was successfully created.')
+    copy = Program.last
+    expect(copy.id).to_not eq(program.id)
+    expect(copy.title).to eq("#{program.title} copy")
+    expect(copy.user_id).to_not eq(program.user_id)
+    changed_attributes = %w(id title user_id created_at updated_at)
+    expect(copy.attributes.except(*changed_attributes)).to eq(program.attributes.except(*changed_attributes))
+    expect(copy.activities.length).to eq(program.activities.length)
+    copy.activities.each do |copy_activity|
+      program_activity = program.activities.find_by(dance_id: copy_activity.dance_id, text: copy_activity.text.presence)
+      expect(program_activity).to be_a(Activity)
+    end
   end
-
-  it "copies the program"
 end
