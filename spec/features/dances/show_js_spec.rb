ccf2c2b895e0edee4380e09d7f65670b42d57aa6
Dave Morse <dcmorse@gmail.com>
2018-12-26 08:31:44 -0600

    toggles load from db

--- spec/features/dances/show_js_spec.rb
+++ spec/features/dances/show_js_spec.rb
@@ -42,9 +42,6 @@ describe 'Showing dances', js: true do
         with_login do |user|
           tag = Tag.find_by(name: 'verified')
 
-          # handle = Capybara.page.driver.current_window_handle
-          # Capybara.page.driver.resize_window_to(handle, 800, 1200)
-
           visit dance_path(dance)
 
           expect(page).to_not have_css(".#{tag.glyphicon}")
@@ -60,14 +57,19 @@ describe 'Showing dances', js: true do
           expect(page).to_not have_css('.tag-label-untagged', text: tag.name)
           expect(page).to have_text(tag.name + ' × 1')
           expect(page).to have_text("you #{tag.on_phrase}")
+          sleep(0.125)
+          expect(page).to_not have_css('glyphicon-time')
           expect(Dut.find_by(dance: dance, user: user, tag: tag)).to_not eq(nil)
 
           toggle_tag(tag)
+
           expect(page).to_not have_css(".#{tag.glyphicon}")
           expect(page).to_not have_css('glyphicon-time');
           expect(page).to have_css('.tag-label-untagged', text: tag.name)
           expect(page).to have_text(tag.name + ' × 0')
           expect(page).to have_text(tag.off_sentence)
+          sleep(0.125)
+          expect(page).to_not have_css('glyphicon-time')
           expect(Dut.find_by(dance: dance, user: user, tag: tag)).to eq(nil)
         end
       end
@@ -90,11 +92,37 @@ describe 'Showing dances', js: true do
 
           toggle_tag(tag)
 
+          expect(page).to_not have_css('glyphicon-time')
           expect(page).to have_css(".#{tag.glyphicon}")
           expect(page).to_not have_css('.tag-label-untagged', text: tag.name)
           expect(page).to have_text("#{tag.name} × #{initial_dut_count + 1}")
           expect(page).to_not have_text("2 users have called this transcription")
           expect(page).to have_text("3 users have called this transcription")
+        end
+      end
+
+      it "loads toggled when you've already made a dut" do
+        with_login do |user|
+          tag = Tag.find_by(name: 'verified')
+          expect(tag.on_phrase).to eq("have called this transcription")
+          FactoryGirl.create(:dut, tag: tag, dance: dance, user: user)
+
+          visit dance_path(dance)
+
+          expect(page).to have_css(".#{tag.glyphicon}")
+          expect(page).to_not have_css('.tag-label-untagged', text: tag.name)
+          expect(page).to have_text("#{tag.name} × 1")
+          expect(page).to have_text("you have called this transcription")
+          expect(find(".tag-constellation[data-tag-id='#{tag.id}']").find('input', visible: false)).to be_checked
+
+          toggle_tag(tag)
+
+          expect(page).to_not have_css('glyphicon-time')
+          expect(page).to_not have_css(".#{tag.glyphicon}")
+          expect(page).to have_css('.tag-label-untagged', text: tag.name)
+          expect(page).to have_text("#{tag.name} × 0")
+          expect(page).to have_text(tag.off_sentence)
+          expect(find(".tag-constellation[data-tag-id='#{tag.id}']").find('input', visible: false)).to_not be_checked
         end
       end
     end
