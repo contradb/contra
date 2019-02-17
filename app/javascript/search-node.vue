5361c7efd622ed0fd0654c4e66683bd17dd4b0aa
Dave Morse <dcmorse@gmail.com>
2019-02-17 07:36:56 -0600

    add figure-filter-op select

--- app/javascript/search-node.vue
+++ app/javascript/search-node.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-    This is this figure component and here is a select thing:
-    <select v-on:change='setFigure' v-model='move'>
-      <option v-bind:key="index" v-for='({term, substitution},index) in moveMenu' v-bind:value='term'>{{substitution}}</option>
-
+  <div class='figure-filter'>
+    <select v-model='operator' class='figure-filter-op form-control'>
+      <option>figure</option>
+      <option>formation</option>
+      <option>progression</option>
+      <option>or</option>
+      <option>and</option>
+      <option>&</option>
+      <option>then</option>
+      <option>no</option>
+      <option>not</option>
+      <option>all</option>
+      <option value='count'>number of</option>
+    </select>
+    <select v-on:change='setFigure' v-model='move' class='figure-filter-move form-control'>
+      <option v-bind:key="index" v-for='({term, substitution},index) in moveMenu' v-bind:value='term'>
+        {{substitution}}
+      </option>
     </select>
   </div>
 </template>
@@ -16,6 +29,7 @@ export default {
   data: function () {
     return {
       moveMenu: moveTermsAndSubstitutionsForSelectMenu(this.dialect),
+      operator: this.tree[0],
       move: this.tree[1]
     };
   },
