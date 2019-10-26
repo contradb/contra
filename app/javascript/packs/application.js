280aca62706a886e16942855a41f8463f1b9a8f8
Dave Morse <dcmorse@gmail.com>
2019-10-26 17:39:29 -0500

    restore react-table 6 and hello_react.tsx

--- app/javascript/packs/application.js
+++
@@ -1,8 +0,0 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
-import {foo} from "../foo.ts"
-
-const f = x => x+" robot";
-
-console.log(foo("Hello space banana", "robot"))
--- app/javascript/packs/hello_react.tsx
+++ app/javascript/packs/hello_react.tsx
@@ -1,34 +1,34 @@
+
+
 // Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from "react"
-import ReactDOM from "react-dom"
-import PropTypes from "prop-types"
-import ReactTable from "react-table"
-import "react-table/react-table.css"
-import { Demo } from "../demo"
-import { foo } from "../foo.ts"
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
-const Hello = props => (
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import * as PropTypes from "prop-types"
+import { Demo } from "../demo"
+
+const Hello = (props: {name: string}) => (
   <div>
     Hello {props.name}!<Demo />
   </div>
 )
 
-Hello.defaultProps = {
-  name: "David",
-}
+// Hello.defaultProps = {
+//   name: "David",
+// }
 
-Hello.propTypes = {
-  name: PropTypes.string,
-}
+// Hello.propTypes = {
+//   name: PropTypes.string,
+// }
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
     <Hello name={"math"} />,
     document.body.appendChild(document.createElement("div"))
   )
-
-  foo(5, "wombat")
 })
