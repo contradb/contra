eaebf45347b83860445b975f5405b1ef3a203b2a
Dave Morse <dcmorse@gmail.com>
2019-11-02 22:15:03 -0500

    convert files forgotten in last commit to have rougly dance-related random content

---
+++ app/javascript/makeData.js
@@ -0,0 +1,38 @@
+import namor from 'namor'
+
+const range = len => {
+  const arr = []
+  for (let i = 0; i < len; i++) {
+    arr.push(i)
+  }
+  return arr
+}
+
+const newDance = () => {
+  const statusChance = Math.random()
+  return {
+    title: namor.generate({ words: 1, numbers: 0 }),
+    choreographer: namor.generate({ words: 2, numbers: 0 }),
+    hook: namor.generate({ words: 3, numbers: 0 }),
+    formation: 'improper',
+    user: namor.generate({ words: 2, numbers: 0 }),
+    entered: namor.generate({ numbers: 1 }),
+    updated: namor.generate({ numbers: 1 }),
+    sharing: 'everyone',
+    figures: 'whole dance',
+  }
+}
+
+export default function makeData(...lens) {
+  const makeDataLevel = (depth = 0) => {
+    const len = lens[depth]
+    return range(len).map(d => {
+      return {
+        ...newDance(),
+        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+      }
+    })
+  }
+
+  return makeDataLevel()
+}
