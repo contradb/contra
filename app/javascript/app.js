30b064eaba93108f7030361877cfc8c5a14919cd
Dave Morse <dcmorse@gmail.com>
2019-11-02 22:15:03 -0500

    convert files forgotten in last commit to have rougly dance-related random content

---
+++ app/javascript/app.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useTable } from 'react-table'
+
+import makeData from './makeData'
+
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(
+          (row, i) =>
+            prepareRow(row) || (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+        )}
+      </tbody>
+    </table>
+  )
+}
+
+function App() {
+  const columns = React.useMemo(
+    () => ['Title',
+           'Choreographer',
+           'Hook',
+           'Formation',
+           'User',
+           'Entered',
+           'Updated',
+           'Sharing',
+           'Figures',
+          ].map(x => {
+            return {
+              Header: x,
+              accessor: x.toLowerCase(),
+            }}),
+    []
+  )
+
+  const data = React.useMemo(() => makeData(20), [])
+
+  return (
+
+      <Table columns={columns} data={data} />
+
+  )
+}
+
+export default App
