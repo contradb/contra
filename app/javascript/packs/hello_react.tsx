ac7d98f2f21d0718a608fa69d14089e794e78ff3
Dave Morse <dcmorse@gmail.com>
2019-11-16 16:29:08 -0600

    rename component to 'DanceTable' from 'App'

--- app/javascript/packs/hello_react.tsx
+++ app/javascript/packs/hello_react.tsx
@@ -3,12 +3,12 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 // import './index.css';
-import App from "../app"
+import DanceTable from "../dance-table"
 // import * as serviceWorker from './serviceWorker';
 const root = document.getElementById("root")
 
 console.log("root = ", root)
-ReactDOM.render(<App />, root)
+ReactDOM.render(<DanceTable />, root)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
