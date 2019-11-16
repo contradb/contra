c86572ec0d74716091f231867a1f9e75863f7033
Dave Morse <dcmorse@gmail.com>
2019-11-16 16:24:02 -0600

    embed react-table in fragment

--- app/javascript/app.tsx
+++ app/javascript/app.tsx
@@ -17,32 +17,34 @@ function Table({ columns, data }: { columns: any; data: any }) {
 
   // Render the UI for your table
   return (
-    <table
-      {...getTableProps()}
-      className="table table-bordered table-hover table-condensed dances-table-react"
-    >
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-              })}
+    <>
+      <table
+        {...getTableProps()}
+        className="table table-bordered table-hover table-condensed dances-table-react"
+      >
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </>
   )
 }
 
