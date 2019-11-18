2d6097ecca7fb51fd7476de42ab976872d21d6bd
Dave Morse <dcmorse@gmail.com>
2019-11-17 21:54:06 -0600

    wip column visible buttons change style of visbile-button (also on welcome-index) toggle columns on and off 1 test passes

--- spec/features/welcome/search_spec.rb
+++ spec/features/welcome/search_spec.rb
@@ -12,4 +12,64 @@ describe 'Search page', js: true do
       expect(page).send to_probably, have_link(dance.choreographer.name, href: choreographer_path(dance.choreographer_id))
     end
   end
+  describe 'columns' do
+    let (:dances) {[:dance, :box_the_gnat_contra, :call_me].map {|d| FactoryGirl.create(d)}}
+    it "Clicking vis toggles buttons cause columns to disappear" do
+      dances
+      visit(s_path)
+      %w[Title Choreographer Formation Hook User Entered].each do |col|
+        puts col
+        expect(page).to have_css('.dances-table-react th', text: col)
+        expect(page).to have_css('button.toggle-vis-active', text: col)
+        expect(page).to_not have_css('button.toggle-vis-inactive', text: col)
+        click_button col
+        expect(page).to_not have_css('.dances-table-react th', text: col)
+        expect(page).to have_css('button.toggle-vis-inactive', text: col)
+        expect(page).to_not have_css('button.toggle-vis-active', text: col)
+        click_button col
+        expect(page).to have_css('.dances-table-react th', text: col)
+        expect(page).to have_css('button.toggle-vis-active', text: col)
+        expect(page).to_not have_css('button.toggle-vis-inactive', text: col)
+      end
+      %w[Updated Sharing].each do |col|
+        expect(page).to_not have_css('.dances-table-react th', text: col)
+        expect(page).to_not have_css('button.toggle-vis-active', text: col)
+        expect(page).to  have_css('button.toggle-vis-inactive', text: col)
+        click_button col
+        expect(page).to have_css('.dances-table-react th', text: col)
+        expect(page).to_not have_css('button.toggle-vis-inactive', text: col)
+        expect(page).to have_css('button.toggle-vis-active', text: col)
+        click_button col
+        expect(page).to_not have_css('.dances-table-react th', text: col)
+        expect(page).to have_css('button.toggle-vis-inactive', text: col)
+        expect(page).to_not have_css('button.toggle-vis-active', text: col)
+      end
+    end
+
+
+    it 'published column cells' do
+      with_login do |user|
+        dances.each_with_index do |dance, i|
+          publish = [:off, :link, :all][i]
+          publish_string = ['myself', 'link', 'everyone'][i]
+          dance.update!(publish: publish, user: user)
+          visit(s_path)
+          click_button 'Sharing'
+          expect(page).to have_css('tr', text: /#{dance.title}.*#{publish_string}/)
+        end
+      end
+    end
+
+    it 'figures column' do
+      dances
+      visit(s_path)
+      expect(page).to_not have_content('whole dance')
+      click_button 'Figures'
+      expect(page).to have_content('whole dance', count: 3)
+      select('circle')
+      expect(page).to have_css('tr', text: /The Rendevouz.*circle left 4 places\ncircle left 3 places/)
+      expect(page).to have_css('tr', text: /Call Me.*circle left 3 places/)
+    end
+  end
+
 end
