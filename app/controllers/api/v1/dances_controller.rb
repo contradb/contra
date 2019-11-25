d45a4f7e7fecadf90217b86295cbb190bbbd349a
Dave Morse <dcmorse@gmail.com>
2019-11-24 23:30:16 -0600

    pagination

--- app/controllers/api/v1/dances_controller.rb
+++ app/controllers/api/v1/dances_controller.rb
@@ -2,11 +2,23 @@ require 'filter_dances'
 
 class Api::V1::DancesController < ApplicationController
   def index
-    render json: FilterDances.filter_dances(filter, count: 10, offset: 0, dialect: dialect)
+    render json: FilterDances.filter_dances(filter, count: count, offset: offset, dialect: dialect)
   end
 
   private
   def filter
     ['figure', '*']
+  end
+
+  def count
+    parseIntWithDefault(params[:count], 10)
+  end
+
+  def offset
+    parseIntWithDefault(params[:offset], 0)
+  end
+
+  def parseIntWithDefault(s, default)
+    s =~ /^[0-9]+$/ ? Integer(s) : default
   end
 end
