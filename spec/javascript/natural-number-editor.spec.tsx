684914cbe0614cd7befb7ca8eb4c0e01e9c7b8f1
Dave Morse <dcmorse@gmail.com>
2019-12-08 10:35:36 -0600

    NaturalNumberEditor has a class of 'form-control'

--- spec/javascript/natural-number-editor.spec.tsx
+++ spec/javascript/natural-number-editor.spec.tsx
@@ -3,17 +3,45 @@ import { render, fireEvent } from "@testing-library/react"
 
 import NaturalNumberEditor from "../../app/javascript/natural-number-editor"
 
-it("works", () => {
-  const testMessage = "Test Message"
-  let value: number = 250624
-  const setValue = jest.fn((x: number) => {
-    value = x
+describe("NaturalNumberEditor", () => {
+  let value: number
+  let setValue: any
+
+  beforeEach(() => {
+    value = 250624
+    setValue = jest.fn((x: number) => {
+      value = x
+    })
   })
-  const { getByDisplayValue } = render(
-    <NaturalNumberEditor value={value} setValue={setValue} />
-  )
-  const field = getByDisplayValue("250624")
-  fireEvent.change(field, { target: { value: 36 } })
-  expect(setValue).toHaveBeenCalledWith(36)
-  expect(setValue).toHaveBeenCalledTimes(1)
+
+  it("works", () => {
+    const { getByDisplayValue } = render(
+      <NaturalNumberEditor value={value} setValue={setValue} />
+    )
+    const field = getByDisplayValue(value.toString())
+    fireEvent.change(field, { target: { value: 36 } })
+    expect(setValue).toHaveBeenCalledWith(36)
+    expect(setValue).toHaveBeenCalledTimes(1)
+  })
+
+  describe("className", () => {
+    it("renders with .form-control", () => {
+      const field = render(
+        <NaturalNumberEditor value={value} setValue={setValue} />
+      ).getByDisplayValue(value.toString())
+      expect(field).toHaveClass("form-control")
+    })
+
+    it("appends .form-control to whatever they pass in in inputProperties", () => {
+      const field = render(
+        <NaturalNumberEditor
+          value={value}
+          setValue={setValue}
+          inputProperties={{ className: "fuzzy-wuzzy" }}
+        />
+      ).getByDisplayValue(value.toString())
+      expect(field).toHaveClass("fuzzy-wuzzy")
+      expect(field).toHaveClass("form-control")
+    })
+  })
 })
