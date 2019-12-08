13903bf40e52b94c5c2aa47e3e79457fd76bbef7
Dave Morse <dcmorse@gmail.com>
2019-12-08 09:01:56 -0600

    always import jest-dom/extend-expect

--- spec/javascript/natural-number-editor.spec.tsx
+++ spec/javascript/natural-number-editor.spec.tsx
@@ -1,11 +1,6 @@
 import React from "react"
 import { render, fireEvent } from "@testing-library/react"
 
-// these imports are something you'd normally configure Jest to import for you
-// automatically. Learn more in the setup docs: https://testing-library.com/docs/react-testing-library/setup#cleanup
-import "@testing-library/jest-dom/extend-expect"
-// NOTE: jest-dom adds handy assertions to Jest and is recommended, but not required
-
 import NaturalNumberEditor from "../../app/javascript/natural-number-editor"
 
 it("works", () => {
