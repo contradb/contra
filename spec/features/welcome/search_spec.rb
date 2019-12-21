6a140ae4f8a534441cf88d860bdd9581e9b82b73
Dave Morse <dcmorse@gmail.com>
2019-12-21 12:34:47 -0600

    Legible dates. Rachet up cell typing.

--- spec/features/welcome/search_spec.rb
+++ spec/features/welcome/search_spec.rb
@@ -5,12 +5,13 @@ require 'rails_helper'
 describe 'Search page', js: true do
   let (:now) { DateTime.now }
   it "works" do
-    dances = 12.times.map {|i| FactoryGirl.create(:dance, title: "Dance #{i}.", created_at: now - i.hours)}
+    dances = 12.times.map {|i| FactoryGirl.create(:dance, title: "Dance #{i}.", created_at: now - i.days)}
     visit(s_path)
     dances.each_with_index do |dance, i|
       to_probably = i < 10 ? :to : :to_not
       expect(page).send to_probably, have_link(dance.title, href: dance_path(dance))
       expect(page).send to_probably, have_link(dance.choreographer.name, href: choreographer_path(dance.choreographer_id))
+      expect(page).send to_probably, have_text(dance.created_at.strftime("%-m/%-d/%Y"))
     end
   end
 
