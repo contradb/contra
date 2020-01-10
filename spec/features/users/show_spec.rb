9f4e94e556bde10c22bea7397fe656dac3d2ef95
Dave Morse <dcmorse@gmail.com>
2020-01-09 21:47:09 -0600

    introduce sketchbook terminology. Sketchbook only renders on user page if it has dances.

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -9,7 +9,18 @@ describe "user show" do
     end
     visit user_path(user)
     expect(page).to_not have_css("table a", text: dances[:off].title)
-    expect(page).to have_css("table.reclusive a", text: dances[:link].title)
-    expect(page).to have_css("table.featured a", text: dances[:all].title)
+    expect(page).to have_css(".public-dances table a", text: dances[:all].title)
+    expect(page).to have_css('.sketchbook-dances h3', text: 'Sketchbook')
+    expect(page).to have_css('.sketchbook-dances p', text: "This is for dances that aren't ready to call. They're not discoverable from the main dance search page.")
+    expect(page).to have_css(".sketchbook-dances table a", text: dances[:link].title)
+    # TODO: show headers to explain why 2 types of dance tables
+    # TODO: only show 2nd table & header if it is non-empty
+  end
+
+  it "doesn't show the sketchbook if the user doesn't have one" do
+    user = FactoryGirl.create(:user)
+    visit user_path(user)
+    expect(page).to_not have_text("Sketchbook")
+    expect(page).to_not have_css(".sketchbook-dances")
   end
 end
