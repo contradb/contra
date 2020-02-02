9696517ae63f395d92143f95812e6f52b113be46
Dave Morse <dcmorse@gmail.com>
2020-02-02 15:03:54 -0600

    flesh out Filter type, including NumericFilter subtype

--- app/javascript/filter.ts
+++ app/javascript/filter.ts
@@ -1,4 +1,27 @@
-// placeholder for now
-export type Filter = Array<Filter | string | number>
+type FigureFilterParameter = number | string | boolean // a guess definition, haven't researched it
+
+export type Comparison = "≥" | "≤" | "≠" | "=" | ">" | "<"
+
+export type Filter =
+  | ["figure", string, ...FigureFilterParameter]
+  | ["&", ...Filter]
+  | ["and", ...Filter]
+  | ["or", ...Filter]
+  | ["then", ...Filter]
+  | ["not", Filter]
+  | ["no", Filter]
+  | ["formation", string]
+  | ["progression"]
+  | ["if", Filter, Filter]
+  | ["if", Filter, Filter, Filter]
+  | ["all", Filter]
+  | ["choreographer", string]
+  | ["count", Filter, Comparison, number]
+  | ["compare", NumericFilter, Comparison, NumericFilter]
+
+export type NumericFilter =
+  | ["constant", number]
+  | ["tag", string]
+  | ["count-matches", Filter]
 
 export default Filter
