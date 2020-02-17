c2cd44d8d1d7f9426b13077314a92e260e64c720
Dave Morse <dcmorse@gmail.com>
2020-02-16 23:29:00 -0600

    search tabs - wip - no tests

--- app/javascript/advanced-search.tsx
+++ app/javascript/advanced-search.tsx
@@ -4,6 +4,7 @@ import Cookie from "js-cookie"
 import DanceTable from "./dance-table"
 import EzCheckboxFilter from "./ez-checkbox-filter"
 import Filter from "./filter"
+import SearchTabs from "./search-tabs"
 import {
   getVerifiedFilter,
   getPublishFilter,
@@ -59,116 +60,126 @@ export const AdvancedSearch = (): JSX.Element => {
 
   return (
     <div>
-      <br />
-      <h4>Choreographer:</h4>
-      <input
-        type="text"
-        className="ez-choreographer-filter form-control"
-        style={{ maxWidth: "15em" }}
-        value={choreographer}
-        onChange={e => setChoreographer(e.target.value)}
-        title="the person who wrote the dance - optional"
+      <SearchTabs
+        initialIndex={2}
+        tabs={[
+          {
+            name: "filters",
+            body: (
+              <div>
+                <h4>Choreographer:</h4>
+                <input
+                  type="text"
+                  className="ez-choreographer-filter form-control"
+                  style={{ maxWidth: "15em" }}
+                  value={choreographer}
+                  onChange={e => setChoreographer(e.target.value)}
+                  title="the person who wrote the dance - optional"
+                />
+                <br />
+                <h4>Hook:</h4>
+                <input
+                  type="text"
+                  className="ez-hook-filter form-control"
+                  style={{ maxWidth: "15em" }}
+                  value={hook}
+                  onChange={e => setHook(e.target.value)}
+                  title="search for words in reason the dance is interesting - optional"
+                />
+                <br />
+                <br />
+                <h4>Verified:</h4>
+                <EzCheckboxFilter
+                  checked={verifiedChecked}
+                  setChecked={setVerifiedChecked}
+                  name="verified"
+                  title="search among dances that have been called"
+                />
+                <EzCheckboxFilter
+                  checked={notVerifiedChecked}
+                  setChecked={setNotVerifiedChecked}
+                  name="not verified"
+                  title="search among dances that have not been called "
+                />
+                <EzCheckboxFilter
+                  checked={verifiedCheckedByMe}
+                  setChecked={setVerifiedCheckedByMe}
+                  disabledReason={signedIn ? null : "must be logged in"}
+                  name="verified by me"
+                  title="search among dances that you have called"
+                />
+                <EzCheckboxFilter
+                  checked={notVerifiedCheckedByMe}
+                  setChecked={setNotVerifiedCheckedByMe}
+                  disabledReason={signedIn ? null : "must be logged in"}
+                  name="not verified by me"
+                  title="search among dances you have not called"
+                />
+                <br />
+                <br />
+                <h4>Shared:</h4>
+                <EzCheckboxFilter
+                  checked={publishAll}
+                  setChecked={setPublishAll}
+                  name="shared"
+                  title="search among dances shared to all"
+                />
+                <EzCheckboxFilter
+                  checked={publishSketchbook}
+                  setChecked={setPublishSketchbook}
+                  name="sketchbooks"
+                  title="search among dances shared to sketchbooks - the person entering them does not think they should be called"
+                />
+                {isAdmin && (
+                  <EzCheckboxFilter
+                    checked={publishOff}
+                    setChecked={setPublishOff}
+                    name="private"
+                    title="search among dances that have sharing off"
+                  />
+                )}
+                <EzCheckboxFilter
+                  checked={enteredByMe}
+                  setChecked={setEnteredByMe}
+                  disabledReason={signedIn ? null : "must be logged in"}
+                  name="entered by me"
+                  title="search among dances you have entered"
+                />
+                <br />
+                <br />
+                <h4>Formation:</h4>
+                <EzCheckboxFilter
+                  checked={improper}
+                  setChecked={setImproper}
+                  name="improper"
+                  title="search among duple improper contras"
+                />
+                <EzCheckboxFilter
+                  checked={becket}
+                  setChecked={setBecket}
+                  name="becket"
+                  title="search among Becket contras"
+                />
+                <EzCheckboxFilter
+                  checked={proper}
+                  setChecked={setProper}
+                  name="proper"
+                  title="search among duple proper contras"
+                />
+                <EzCheckboxFilter
+                  checked={otherFormation}
+                  setChecked={setOtherFormation}
+                  name="everything else"
+                  title="search among all the other formations"
+                />
+              </div>
+            ),
+          },
+          { name: "query", body: <div>Coming Soon!</div> },
+          { name: "results", body: <DanceTable filter={grandFilter} /> },
+          { name: "program", body: <div>Coming Eventually!</div> },
+        ]}
       />
-      <br />
-      <h4>Hook:</h4>
-      <input
-        type="text"
-        className="ez-hook-filter form-control"
-        style={{ maxWidth: "15em" }}
-        value={hook}
-        onChange={e => setHook(e.target.value)}
-        title="search for words in reason the dance is interesting - optional"
-      />
-      <br />
-      <br />
-      <h4>Verified:</h4>
-      <EzCheckboxFilter
-        checked={verifiedChecked}
-        setChecked={setVerifiedChecked}
-        name="verified"
-        title="search among dances that have been called"
-      />
-      <EzCheckboxFilter
-        checked={notVerifiedChecked}
-        setChecked={setNotVerifiedChecked}
-        name="not verified"
-        title="search among dances that have not been called "
-      />
-      <EzCheckboxFilter
-        checked={verifiedCheckedByMe}
-        setChecked={setVerifiedCheckedByMe}
-        disabledReason={signedIn ? null : "must be logged in"}
-        name="verified by me"
-        title="search among dances that you have called"
-      />
-      <EzCheckboxFilter
-        checked={notVerifiedCheckedByMe}
-        setChecked={setNotVerifiedCheckedByMe}
-        disabledReason={signedIn ? null : "must be logged in"}
-        name="not verified by me"
-        title="search among dances you have not called"
-      />
-      <br />
-      <br />
-      <h4>Shared:</h4>
-      <EzCheckboxFilter
-        checked={publishAll}
-        setChecked={setPublishAll}
-        name="shared"
-        title="search among dances shared to all"
-      />
-      <EzCheckboxFilter
-        checked={publishSketchbook}
-        setChecked={setPublishSketchbook}
-        name="sketchbooks"
-        title="search among dances shared to sketchbooks - the person entering them does not think they should be called"
-      />
-      {isAdmin && (
-        <EzCheckboxFilter
-          checked={publishOff}
-          setChecked={setPublishOff}
-          name="private"
-          title="search among dances that have sharing off"
-        />
-      )}
-      <EzCheckboxFilter
-        checked={enteredByMe}
-        setChecked={setEnteredByMe}
-        disabledReason={signedIn ? null : "must be logged in"}
-        name="entered by me"
-        title="search among dances you have entered"
-      />
-      <br />
-      <br />
-      <h4>Formation:</h4>
-      <EzCheckboxFilter
-        checked={improper}
-        setChecked={setImproper}
-        name="improper"
-        title="search among duple improper contras"
-      />
-      <EzCheckboxFilter
-        checked={becket}
-        setChecked={setBecket}
-        name="becket"
-        title="search among Becket contras"
-      />
-      <EzCheckboxFilter
-        checked={proper}
-        setChecked={setProper}
-        name="proper"
-        title="search among duple proper contras"
-      />
-      <EzCheckboxFilter
-        checked={otherFormation}
-        setChecked={setOtherFormation}
-        name="everything else"
-        title="search among all the other formations"
-      />
-      <br />
-      <br />
-      <br />
-      <DanceTable filter={grandFilter} />
     </div>
   )
 }
---
+++ app/javascript/search-tabs.tsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+import { useState } from "react"
+
+export type SearchTab = { name: string; body: JSX.Element }
+
+export const SearchTabs = ({
+  tabs,
+  initialIndex,
+}: {
+  tabs: SearchTab[]
+  initialIndex: number
+}): JSX.Element => {
+  const [selectedIndex, setSelectedIndex] = useState(initialIndex)
+  return (
+    <>
+      <div className="search-tabs">
+        {tabs.map((tab, index) => (
+          <a
+            onClick={() => setSelectedIndex(index)}
+            className={index === selectedIndex ? "selected" : ""}
+            key={index}
+          >
+            {tab.name}
+          </a>
+        ))}
+      </div>
+      {tabs.map((tab, index) => {
+        const maybeHidden = index === selectedIndex ? "" : "hidden"
+        return <div className={maybeHidden}>{tab.body}</div>
+      })}
+    </>
+  )
+}
+
+export default SearchTabs
