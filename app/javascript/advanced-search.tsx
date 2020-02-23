6285a6c2c24e4fe359fbd53c2c8a4dc0821c80ec
Dave Morse <dcmorse@gmail.com>
2020-02-22 23:26:24 -0600

    split *-tab.tsx out of advanced-search.tsx so we can paste them into the right screen layout based on device type

--- app/javascript/advanced-search.tsx
+++ app/javascript/advanced-search.tsx
@@ -1,22 +1,19 @@
-import * as React from "react"
-import { useState, useCallback } from "react"
-import Cookie from "js-cookie"
+import React, { useState, useCallback } from "react"
 import {
-  DanceTable,
   FetchDataFn,
   sortByParam,
   SearchDancesDanceJson,
   SearchDancesJson,
 } from "./dance-table"
-import EzCheckboxFilter from "./ez-checkbox-filter"
 import Filter from "./filter"
 import useFilter from "use-filter"
 import SearchTabs from "./search-tabs"
+import FiltersTab from "./filters-tab"
+import FiguresTab from "./figures-tab"
+import DancesTab from "./dances-tab"
+import ProgramTab from "./program-tab"
 
 export const AdvancedSearch = (): JSX.Element => {
-  const [signedIn] = useState(() => Cookie.get("signed_in"))
-  const isAdmin = signedIn === "admin"
-
   const [searchDancesJson, setSearchDancesJson] = useState({
     dances: [] as SearchDancesDanceJson[],
     numberSearched: 0,
@@ -52,7 +49,9 @@ export const AdvancedSearch = (): JSX.Element => {
     []
   )
   const { filter, dictionary } = useFilter()
-  const d: any = dictionary
+  const dancesTabName: string = `${searchDancesJson.numberMatching} dance${
+    1 === searchDancesJson.numberMatching ? "" : "s"
+  }`
   return (
     <div>
       <SearchTabs
@@ -60,138 +59,23 @@ export const AdvancedSearch = (): JSX.Element => {
         tabs={[
           {
             name: "filters",
-            body: (
-              <div>
-                <h4>Choreographer:</h4>
-                <input
-                  type="text"
-                  className="ez-choreographer-filter form-control"
-                  style={{ maxWidth: "15em" }}
-                  value={d.choreographer}
-                  onChange={e => d.setChoreographer(e.target.value)}
-                  title="the person who wrote the dance - optional"
-                />
-                <br />
-                <h4>Hook:</h4>
-                <input
-                  type="text"
-                  className="ez-hook-filter form-control"
-                  style={{ maxWidth: "15em" }}
-                  value={d.hook}
-                  onChange={e => d.setHook(e.target.value)}
-                  title="search for words in reason the dance is interesting - optional"
-                />
-                <br />
-                <br />
-                <h4>Verified:</h4>
-                <EzCheckboxFilter
-                  checked={d.verifiedChecked}
-                  setChecked={d.setVerifiedChecked}
-                  name="verified"
-                  title="search among dances that have been called"
-                />
-                <EzCheckboxFilter
-                  checked={d.notVerifiedChecked}
-                  setChecked={d.setNotVerifiedChecked}
-                  name="not verified"
-                  title="search among dances that have not been called "
-                />
-                <EzCheckboxFilter
-                  checked={d.verifiedCheckedByMe}
-                  setChecked={d.setVerifiedCheckedByMe}
-                  disabledReason={signedIn ? null : "must be logged in"}
-                  name="verified by me"
-                  title="search among dances that you have called"
-                />
-                <EzCheckboxFilter
-                  checked={d.notVerifiedCheckedByMe}
-                  setChecked={d.setNotVerifiedCheckedByMe}
-                  disabledReason={signedIn ? null : "must be logged in"}
-                  name="not verified by me"
-                  title="search among dances you have not called"
-                />
-                <br />
-                <br />
-                <h4>Shared:</h4>
-                <EzCheckboxFilter
-                  checked={d.publishAll}
-                  setChecked={d.setPublishAll}
-                  name="shared"
-                  title="search among dances shared to all"
-                />
-                <EzCheckboxFilter
-                  checked={d.publishSketchbook}
-                  setChecked={d.setPublishSketchbook}
-                  name="sketchbooks"
-                  title="search among dances shared to sketchbooks - the person entering them does not think they should be called"
-                />
-                {isAdmin && (
-                  <EzCheckboxFilter
-                    checked={d.publishOff}
-                    setChecked={d.setPublishOff}
-                    name="private"
-                    title="search among dances that have sharing off"
-                  />
-                )}
-                <EzCheckboxFilter
-                  checked={d.enteredByMe}
-                  setChecked={d.setEnteredByMe}
-                  disabledReason={signedIn ? null : "must be logged in"}
-                  name="entered by me"
-                  title="search among dances you have entered"
-                />
-                <br />
-                <br />
-                <h4>Formation:</h4>
-                <EzCheckboxFilter
-                  checked={d.improper}
-                  setChecked={d.setImproper}
-                  name="improper"
-                  title="search among duple improper contras"
-                />
-                <EzCheckboxFilter
-                  checked={d.becket}
-                  setChecked={d.setBecket}
-                  name="becket"
-                  title="search among Becket contras"
-                />
-                <EzCheckboxFilter
-                  checked={d.proper}
-                  setChecked={d.setProper}
-                  name="proper"
-                  title="search among duple proper contras"
-                />
-                <EzCheckboxFilter
-                  checked={d.otherFormation}
-                  setChecked={d.setOtherFormation}
-                  name="everything else"
-                  title="search among all the other formations"
-                />
-              </div>
-            ),
+            body: <FiltersTab dictionary={dictionary} />,
           },
-          { name: "figures", body: <div>Coming Soon!</div> },
+          { name: "figures", body: <FiguresTab /> },
           {
-            name: `${searchDancesJson.numberMatching} dance${
-              1 === searchDancesJson.numberMatching ? "" : "s"
-            }`,
+            name: dancesTabName,
             loading: loading,
             body: (
-              <>
-                {loading && (
-                  <div className="floating-loading-indicator">loading...</div>
-                )}
-
-                <DanceTable
-                  filter={filter}
-                  fetchDataFn={fetchDataFn}
-                  searchDancesJson={searchDancesJson}
-                  pageCount={pageCount}
-                />
-              </>
+              <DancesTab
+                loading={loading}
+                filter={filter}
+                fetchDataFn={fetchDataFn}
+                searchDancesJson={searchDancesJson}
+                pageCount={pageCount}
+              />
             ),
           },
-          { name: "program", body: <div>Coming Eventually!</div> },
+          { name: "program", body: <ProgramTab /> },
         ]}
       />
     </div>
