cd188db9cb7c2dc443ebe40b4c191ac7b2644aa1
Dave Morse <dcmorse@gmail.com>
2020-03-04 08:25:21 -0600

    remove useless div

--- app/javascript/open-side-tab-toggle.tsx
+++ app/javascript/open-side-tab-toggle.tsx
@@ -13,19 +13,16 @@ export const OpenSideTabToggle = ({
     ? "side-panel-toggle-open-right"
     : "side-panel-toggle-open-left"
   const glyphicon = onRightSide ? "glyphicon-menu-right" : "glyphicon-menu-left"
+  const classes = `side-panel-toggle side-panel-toggle-open ${sidePanelToggleOpenSide}`
   const button = (
-    <div>
-      <div
-        className={`side-panel-toggle side-panel-toggle-open ${sidePanelToggleOpenSide}`}
-      >
-        <button className={buttonClass} onClick={onToggle}>
-          <h2>
-            {onRightSide ? <span>{label}</span> : null}
-            <span className={"glyphicon " + glyphicon} />
-            {onRightSide ? null : <span>{label}</span>}
-          </h2>
-        </button>
-      </div>
+    <div className={classes}>
+      <button className={buttonClass} onClick={onToggle}>
+        <h2>
+          {onRightSide ? <span>{label}</span> : null}
+          <span className={"glyphicon " + glyphicon} />
+          {onRightSide ? null : <span>{label}</span>}
+        </h2>
+      </button>
     </div>
   )
   return button
