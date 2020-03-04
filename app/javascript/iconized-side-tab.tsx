195d582575f4fcea7338022cfd99020487299d3d
Dave Morse <dcmorse@gmail.com>
2020-03-04 08:24:55 -0600

    add IconizedSideTab which was sitting unadded to the repo for many commits git add app/javascript/iconized-side-tab.tsx !

---
+++ app/javascript/iconized-side-tab.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+export const IconizedSideTab = ({
+  label,
+  onToggle,
+}: {
+  label: "program" | "filters"
+  onToggle: () => void
+}): JSX.Element => {
+  const onRightSide = label === "program"
+  const sideClass = onRightSide
+    ? "side-panel-toggle-iconized-right"
+    : "side-panel-toggle-iconized-left"
+  const buttonClass = label === "program" ? "toggle-program" : "toggle-filters"
+  const glyphicon = onRightSide ? "glyphicon-menu-left" : "glyphicon-menu-right"
+  let labelJSX = []
+  for (let i = 0; i < label.length; i++)
+    labelJSX.push(<div key={i}>{label.charAt(i)}</div>)
+  const classes = `side-panel-toggle side-panel-toggle-iconized ${sideClass}`
+  const button = (
+    <div className={classes}>
+      <button className={buttonClass} onClick={onToggle}>
+        <h2>
+          <div>
+            <span className={"glyphicon " + glyphicon} />
+          </div>
+          {labelJSX}
+        </h2>
+      </button>
+    </div>
+  )
+  return button
+}
+
+export default IconizedSideTab
