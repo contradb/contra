bdfc6e93fe865a29658e7b9670223f30c9a4632d
Dave Morse <dcmorse@gmail.com>
2020-04-16 08:47:36 -0500

    refactor FigureSearchEx constructor

--- app/javascript/search-ex.js
+++ app/javascript/search-ex.js
@@ -269,38 +269,56 @@ class FigureSearchEx extends nullaryMixin(SearchEx) {
       parameters = src && src.move === move ? [...src.parameters] : [],
       ellipsis = src ? src.ellipsis : parameters && parameters.length > 0,
     } = args
+    const dealiasedMove = this.maybeDealiasMove(move, parameters)
+    this._move = dealiasedMove
+    this._ellipsis = ellipsis
+    this._parameters = this.initialParameters(
+      dealiasedMove,
+      ellipsis,
+      parameters
+    )
+  }
+
+  // private & functional
+  maybeDealiasMove(move, parameters) {
     const isAlias = move !== "*" && LibFigure.isAlias(move)
-    const aliasFilter = isAlias ? LibFigure.aliasFilter(move) : []
-    let massagedMove = move
     if (isAlias) {
-      // set move to something less specific, if the params are wider
+      const aliasFilter = LibFigure.aliasFilter(move)
+      // if the parameters have a wider domain than the alias, dealias it
       for (let i = 0; i < parameters.length && i < aliasFilter.length; i++) {
         const parameter = parameters[i]
         const aliasF = aliasFilter[i]
-        if (parameter != aliasF && aliasF != "*") {
-          massagedMove = LibFigure.deAliasMove(move)
-          break
-        }
+        if (parameter != aliasF && aliasF != "*")
+          return LibFigure.deAliasMove(move)
       }
     }
-    this._move = massagedMove
-    this._parameters = parameters
-    this._ellipsis = ellipsis
+    return move
+  }
 
-    // initialize parameters
-    if (0 === parameters.length && ellipsis && this.move !== "*") {
-      const formals = LibFigure.formalParameters(this.move)
-      this._parameters = [...this.parameters]
-      while (this.parameters.length < formals.length) {
-        if (this.parameters.length < aliasFilter.length) {
-          this.parameters.push(aliasFilter[this.parameters.length])
+  // private & functional
+  initialParameters(move, ellipsis, parameters) {
+    if (ellipsis && 0 === parameters.length && move !== "*") {
+      const formals = LibFigure.formalParameters(move)
+      const freshParameters = [...parameters]
+      const isAlias = move !== "*" && LibFigure.isAlias(move)
+      const aliasFilter = isAlias && LibFigure.aliasFilter(move)
+      while (freshParameters.length < formals.length) {
+        const isTextParameter =
+          formals[freshParameters.length].ui.name == "chooser_text"
+        let parameter
+        if (isAlias) {
+          parameter = aliasFilter[freshParameters.length]
+        } else if (isTextParameter) {
+          // Since they may want to search for the literal text '*',
+          // we encode searching for any text as the empty string.
+          parameter = ""
         } else {
-          const formalIsText =
-            formals[this.parameters.length].ui.name == "chooser_text"
-          this.parameters.push(formalIsText ? "" : "*")
+          parameter = "*"
         }
+        freshParameters.push(parameter)
       }
-    }
+      return freshParameters
+    } else return parameters
   }
 
   toLisp() {
