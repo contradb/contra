1a873b07d233c4e2e92224e3f8686994bc25337c
Dave Morse <magnet-dev+dm@asynchrony.com>
2020-05-08 17:45:08 -0500

    puma now serves all files (since we're not using nginx in kube)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true # ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Don't Compress JavaScripts - angular likes unmunged variable names. 
   config.assets.js_compressor = Uglifier.new(mangle: false) # was: :uglifier
