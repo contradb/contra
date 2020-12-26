c5164859f4ed56d93a514f3f42f4ae2c6446123f
Dave Morse <dcmorse@gmail.com>
2020-12-26 16:20:49 -0500

    rename one-ec2.tf to ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,86 @@
+variable "ssh_public_key_path" {
+  default = "~/.ssh/contradb-terraform.pub"
+}
+variable "ssh_private_key_path" {
+  default = "~/.ssh/contradb-terraform"
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "server" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.nano"
+  key_name = aws_key_pair.contra_key.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id = aws_subnet.web.id
+
+  tags = {
+    Name = "contradb"
+  }
+
+#   connection {
+#     type = "ssh"
+#     host = self.public_ip
+#     user = "ec2-user"
+#     private_key = file(var.ssh_private_key_path)
+#   }
+
+#   provisioner "remote-exec" {
+#     inline = ["echo hello world"]
+#   }
+
+  # delete_on_termination = eventually false, but for now true is aok
+  # might need a aws_network_interface, but probably can get away with the network_interface block
+}
+
+resource "aws_key_pair" "contra_key" {
+  key_name   = "contradb-terraform-key"
+  public_key = file(var.ssh_public_key_path)
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id = aws_vpc.contra_vpc.id
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+output "ip" {
+  value = aws_eip.web_ip.public_ip
+}
+output "domain" {
+  value = aws_eip.web_ip.public_dns
+}
